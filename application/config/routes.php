16dcd565889d5d639408c50e3b864fae0834c757
rizkiselian <85470186+rizkiselian@users.noreply.github.com>
2021-11-01 10:53:40 +0700

    new

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,9 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'Auth';
 
-// master
-$route['Merek'] = 'merek';
-
-
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/models/M_formulir.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_formulir extends CI_Model {
+	
+	
+}
+
+/* End of file M_admin.php */
+/* Location: ./application/models/M_admin.php */
